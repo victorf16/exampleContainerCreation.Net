d6f210efacd13703ea41c0ea9e0fc9103f894c70
Vitao <victorf_16@hotmail.com>
2023-10-27 00:07:47 -0300

    Updating Last Verstion

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/* Creating Scripts Container IN azure Storage account */
+
 using Azure.Storage.Blobs;
 
 Console.WriteLine("Hello, World!");
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=vitaostoragecool;AccountKey=uJO6I6ZfPP2OstDo5bbx5s8wCfmAjEGQ/0Qf2ZKBV4N169Kw0gu0J8ONhLlgfsxhRgDVZtxHOCQe+ASt20aNGg==;EndpointSuffix=core.windows.net";
-string containerName = "scripts";
+string containerName = "scripts1";
 
 BlobServiceClient blobService
      = new BlobServiceClient(connectionString);
