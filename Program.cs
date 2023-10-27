1cf62960ec4b47610950eb4a8befa35b6292875a
Vitao <victorf_16@hotmail.com>
2023-10-27 00:00:27 -0300

    First COmmit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Blobs;
+
+Console.WriteLine("Hello, World!");
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=vitaostoragecool;AccountKey=uJO6I6ZfPP2OstDo5bbx5s8wCfmAjEGQ/0Qf2ZKBV4N169Kw0gu0J8ONhLlgfsxhRgDVZtxHOCQe+ASt20aNGg==;EndpointSuffix=core.windows.net";
+string containerName = "scripts";
+
+BlobServiceClient blobService
+     = new BlobServiceClient(connectionString);
+await blobService.CreateBlobContainerAsync(containerName);
+Console.WriteLine("Container Created");
+
